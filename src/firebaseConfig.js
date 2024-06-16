5fe6eb3644a8e1e3ffaef85e177819b3b51e27d4
JieWun Lu <syncmaster79281@gmail.com>
2024-06-16 15:08:08 +0800

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,15 +2,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAXjraor51lUadglu_ppLb4bqN9OPpbc5A",
-  authDomain: "izakaya-1711424542019.firebaseapp.com",
-  projectId: "izakaya-1711424542019",
-  storageBucket: "izakaya-1711424542019.appspot.com",
-  messagingSenderId: "599467736545",
-  appId: "1:599467736545:web:42d95b0f20262b2f0626a0",
-  measurementId: "G-QLKRY5LF5M"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
